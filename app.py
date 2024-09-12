81cdf64fad3e037bf3547317bef0dce03c15487e
Pinklad <117762026+pinklad@users.noreply.github.com>
2024-09-12 21:45:56 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+#A project about How much money each coach made and then calculate How much maina mdogo makes and How many clients each coach got
+# "a program that ina itisha input from user apeane value ya macoach wamepata lessons ngapi ie. coach wanafaa kuwa more than 5, output ni inafaa kutoa dooh ya each coach kumulative na pia kenye peter ana get akiokota za macoach wo = {"name": "Max", "Age": 28 }
+
+
+myset = set()
+
+myset.add(1)
+myset.add(2)
+myset.add(3)
+
+print(myset)
---
+++ list.py
@@ -0,0 +1,7 @@
+camping_stuff ="Tent, etehernet cabeles, Usbs, pens , My laptop, Rspberry Pi, marshmallows, cofee, lots of Coffee "
+
+print(camping_stuff)
+
+#python lists
+camping_list = ["Tent, etehernet cabeles, Usbs, pens , My laptop, Rspberry Pi, marshmallows, cofee, lots of Coffee"]
+
